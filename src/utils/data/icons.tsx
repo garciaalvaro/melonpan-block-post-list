1495ad047d59625f8c5d9e4935b8ad7ba7187e6d
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2019-08-29 15:51:32 +0200

    Updated logo.

--- src/utils/data/icons.tsx
+++ src/utils/data/icons.tsx
@@ -27,9 +27,13 @@ export const icons: Icons = {
 	),
 	logo: (
 		<svg width="18" height="18" viewBox="0 0 100 100">
-			<rect width="12" height="80" x="30" y="10" />
-			<rect width="40" height="15" x="30" y="10" />
-			<rect width="40" height="15" x="30" y="75" />
+			<rect width="40" height="15" x="5" y="10" />
+			<rect width="40" height="15" x="5" y="43" />
+			<rect width="12" height="40" x="33" y="10" />
+			<rect width="12" height="80" x="5" y="10" />
+
+			<rect width="12" height="80" x="55" y="10" />
+			<rect width="40" height="15" x="55" y="75" />
 		</svg>
 	)
 };
