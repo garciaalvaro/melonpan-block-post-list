11f43a272fd4e80db755f8efe5fd4aefad5434d4
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2019-12-06 17:05:03 +0900

    Load stylus variables from a file

--- webpack/build.config.babel.js
+++ webpack/build.config.babel.js
@@ -49,7 +49,10 @@ export default {
 						loader: "stylus-loader",
 						options: {
 							use: [nib()],
-							import: ["~nib/index.styl"]
+							import: [
+								"~nib/index.styl",
+								path.join(__dirname, "../src/utils/data/stylus_variables.styl")
+							]
 						}
 					}
 				]
